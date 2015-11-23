ad6a6f70199776a1d2ccac6878bc20ec4fa556e3
Victor94leon <victorlopez94leon@gmail.com>
2015-11-23 11:59:04 +0100

    Update ClockDisplay.java

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,13 +1,13 @@
 /**
- * Imaginemos que tenemos un amigo programador en Java como nosotros que quiere hacer una aplicaciÛn para telÈfonos Android consistente en un reloj 
+ * Imaginemos que tenemos un amigo programador en Java como nosotros que quiere hacer una aplicaci√≥n para tel√©fonos Android consistente en un reloj 
  * en formato 24 horas, que muestre las horas y minutos desde las 00:00 hasta las 23:59.
  *
  *Como anda muy apurado de tiempo, nos pide que le hagamos parte del trabajo. Concretamente necesita que le programemos una clase llamada ClockDisplay que:
  *
- *Permita fijar un momento determinado de tiempo expresado en horas y minutos. Por ejemplo, si yo le indico a un mÈtodo 23 y 45 como par·metros, el tiempo 
- * del reloj debe quedar fijado a 23:45. El nombre del mÈtodo debe ser setTime.
- *Permita hacer avanzar un minuto el tiempo fijado. Evidentemente, el usuario podr· invocar este mÈtodo tantas veces como quiera. El nombre del mÈtodo debe ser timeTick.
- *Devolver una cadena de 5 caracteres consistente en la hora y los minutos separados por dos puntos. El nombre del mÈtodo debe ser getTime. Ojo: la cadena siempre debe
+ *Permita fijar un momento determinado de tiempo expresado en horas y minutos. Por ejemplo, si yo le indico a un m√©todo 23 y 45 como par√°metros, el tiempo 
+ * del reloj debe quedar fijado a 23:45. El nombre del m√©todo debe ser setTime.
+ *Permita hacer avanzar un minuto el tiempo fijado. Evidentemente, el usuario podr√° invocar este m√©todo tantas veces como quiera. El nombre del m√©todo debe ser timeTick.
+ *Devolver una cadena de 5 caracteres consistente en la hora y los minutos separados por dos puntos. El nombre del m√©todo debe ser getTime. Ojo: la cadena siempre debe
  *tener 5 caracteres.
  *
  *Es muy posible que no seas capaz de completar la actividad. No te preocupes, el objetivo es intentarlo.
@@ -25,7 +25,7 @@ public class ClockDisplay
      */
     public ClockDisplay ()
     { 
-        horas = 0;
+        horas = 0:
         minutos = 0;
     }
     
@@ -59,7 +59,7 @@ public class ClockDisplay
 	}
     
     /**
-     * Devuelve una cadena de 5 caracteres mostrando la hora separada de los minutos mediante dos puntos.
+     * Devuelve una cadena de 5 caracteres mostrando la hora separada de los minutos mediante dos puntos..
      */
     public String getTime ()
     {
